a1bdfd57d71cac66f6ce216d8bd6845b50f2fef6
82107 <i79322292@gmail.com>
2023-06-30 13:28:31 +0900

    Tab complete

--- app/src/main/java/com/example/fw5_nmf/MainActivity.java
+++ app/src/main/java/com/example/fw5_nmf/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.fw5_nmf;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs) ;
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int pos = tab.getPosition() ;
+                changeView(pos) ;
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // do nothing
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // do nothing
+            }
+        }) ;
+    }
+    private void changeView(int index) {
+        TextView textView1 = (TextView) findViewById(R.id.text1) ;
+        TextView textView2 = (TextView) findViewById(R.id.text2) ;
+        TextView textView3 = (TextView) findViewById(R.id.text3) ;
+
+        switch (index) {
+            case 0 :
+                textView1.setVisibility(View.VISIBLE) ;
+                textView2.setVisibility(View.INVISIBLE) ;
+                textView3.setVisibility(View.INVISIBLE) ;
+                break ;
+            case 1 :
+                textView1.setVisibility(View.INVISIBLE) ;
+                textView2.setVisibility(View.VISIBLE) ;
+                textView3.setVisibility(View.INVISIBLE) ;
+                break ;
+            case 2 :
+                textView1.setVisibility(View.INVISIBLE) ;
+                textView2.setVisibility(View.INVISIBLE) ;
+                textView3.setVisibility(View.VISIBLE) ;
+                break ;
+
+        }
     }
 }
