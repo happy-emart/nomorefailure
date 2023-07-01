82a5aad0b539529d3d0f1e0a508030d3527867c2
82107 <i79322292@gmail.com>
2023-07-01 16:23:23 +0900

    병합

--- app/src/main/java/com/example/fw5_nmf/Fragment1.java
+++ app/src/main/java/com/example/fw5_nmf/Fragment1.java
@@ -1,23 +1,82 @@
 package com.example.fw5_nmf;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
 public class Fragment1 extends Fragment {
 
+    private ArrayList<String> testDataSet;
     public Fragment1() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_1, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_1, container, false);
+
+        testDataSet = new ArrayList<>();
+        loadJSONData();
+
+        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);  // LayoutManager 설정
+        CustomAdapter customAdapter = new CustomAdapter(testDataSet);
+        recyclerView.setAdapter(customAdapter);
+
+        return view;
+    }
+    private void loadJSONData() {
+        try {
+            String json = loadJSONFromAsset();
+            if (json != null) {
+                JSONArray jsonArray = new JSONArray(json);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject entry = jsonArray.getJSONObject(i);
+                    String name = entry.getString("name");
+                    String number = entry.getString("number");
+                    String data = name + ": " + number;
+                    testDataSet.add(data);
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String loadJSONFromAsset() {
+        String json;
+        try {
+            AssetManager assetManager = requireContext().getAssets();
+            InputStream inputStream = assetManager.open("phone-number.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            json = new String(buffer, StandardCharsets.UTF_8);
+            return json;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
+
